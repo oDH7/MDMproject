b04c266af3a29368c47ac36cfa0617e3be498681
Brito <125681424+GuilhermeBritoBR@users.noreply.github.com>
2023-05-17 15:41:18 -0300

    1.1

---
+++ link.js
@@ -0,0 +1,10 @@
+function acess()   {
+    var senha = document.getElementById("senha");
+    var key={
+        'key_json': ['kf53r1jj'],
+    }
+}
+    if(key.key_json[0] == senha.value ){
+        window.location.assign("index.html");
+        window.location.replace("index.html");
+}
